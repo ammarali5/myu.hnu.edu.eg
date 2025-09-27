80db07e608a238f0f1b24b8d3176a04dd9887d59
ammarali5 <ammaralighazi5@gmail.com>
2025-09-28 02:51:43 +0300

    .

--- home.js
+++ home.js
@@ -32,9 +32,10 @@ function showSection(section) {
   // hide all sections
   homeSection.style.display = "none";
   paymentsSection.style.display = "none";
-
+  
   // show the chosen section
   section.style.display = "block";
+  navDrawer.style.transform = "translateX(-256px)"; // hide
 }
 
 // click events
